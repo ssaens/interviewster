bca664ee5fbfd8a8010e4d7d483fc74ac24b170e
dillon <dilong.yao@gmail.com>
2018-11-24 20:01:32 -0800

    fix cap

--- client/components/ui/index.js
+++ client/components/ui/index.js
@@ -2,7 +2,7 @@ import Button from './button';
 import DropDown from './drop-down';
 import ModalCard from './modal-card';
 import SearchBar from './search-bar';
-import Textarea from './Textarea';
+import Textarea from './textarea';
 import Textbox from './textbox';
 
 export {
